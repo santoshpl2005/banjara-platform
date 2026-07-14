111f3c671b66e77211c3eb041238a3b818871dc4
SANTOSH P LAMANI <santoshplamani2005@gmail.com>
2026-07-14 16:28:11 +0530

    Fix Railway MySQL variables

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 ﻿import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'banjara_secret_2026')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'root123')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'banjara_db')
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    MYSQL_HOST = os.environ.get("MYSQLHOST")
+    MYSQL_USER = os.environ.get("MYSQLUSER")
+    MYSQL_PASSWORD = os.environ.get("MYSQLPASSWORD")
+    MYSQL_DB = os.environ.get("MYSQLDATABASE")
     MYSQL_PORT = int(os.environ.get("MYSQLPORT", 3306))
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'static/images')
+
+    UPLOAD_FOLDER = "static/images"
