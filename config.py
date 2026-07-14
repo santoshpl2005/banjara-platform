bcd796de9cc9687a117d318a89c704d584104f91
SANTOSH P LAMANI <santoshplamani2005@gmail.com>
2026-07-14 15:18:57 +0530

    Update Railway MySQL config

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 ﻿import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'banjara_secret_2024')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'root123')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'banjara_db')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'static/images')
+    SECRET_KEY = os.environ.get("SECRET_KEY", "banjara_secret_2026")
+
+    MYSQL_HOST = os.environ.get("MYSQLHOST", "localhost")
+    MYSQL_USER = os.environ.get("MYSQLUSER", "root")
+    MYSQL_PASSWORD = os.environ.get("MYSQLPASSWORD", "")
+    MYSQL_DB = os.environ.get("MYSQLDATABASE", "railway")
+    MYSQL_PORT = int(os.environ.get("MYSQLPORT", 3306))
+
+    UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER", "static/images")
